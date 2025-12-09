dfe067d4c5a9bee6bc7290e6bff0e08b3979f389
Gouken27 <glaidneri@gmail.com>
2025-12-09 14:38:06 +0700

    add files

---
+++ PROGA/code/Database.hpp
@@ -0,0 +1,57 @@
+#ifndef LAB1_HPP
+#define LAB1_HP
+
+/*1 lab*/
+void DateEaster();
+int exponentiation(float x ,float n);
+void calculationCosinus();
+
+/*2 lab*/
+void inputArrayRecursively();
+void printPlusMinRecursive();
+void convertDecimalToBinary();
+
+/*3 lab*/
+void createAndSplitRandomArray();
+void sortViaPointerArray();
+void sieveOfEratosthenes();
+
+/*4 lab*/
+void removeRowAndColWithMaxElement();
+void expandMatrixWithSums();
+
+/*5 lab*/
+void convertArrayToMatrixWithPadding();
+void createJaggedMultiplicationTable();
+
+/*6 lab*/
+int calculateTriangleMetrics(double a, double b, double c, double* perimeter, double* area);
+void calculateGenderProbability(int n, int m, double& probGirls, double& probBoys);
+
+/*7 lab*/
+void zeroBetweenTwoMins();
+void computeJaggedRowSums();
+void sortDescending();
+
+/*8 lab*/
+void sortSchoolsByAdmissionRate();
+void calculateFacultyStatistics();
+
+/*9 lab*/
+void createAndSortStudentList();
+void createAndFilterStudentDLL();
+void createAndTraverseStudentBST();
+
+/*10 lab*/
+void readSortAndWriteWords();
+
+/*11 lab*/
+void fiileWorks();
+
+/*12 lab*/
+void createSortingLibrary();
+
+/*13 РГР*/
+int RGRwariantInput();
+
+#endif
